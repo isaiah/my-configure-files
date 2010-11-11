8506524efc99d042c8e0de091d9b37908ed4721e
Isaiah <issaria@gmail.com>
2010-11-11 18:23:27 +0800

    switch theme from dust to sky

---
+++ awesome/themes/sky/theme.lua
@@ -0,0 +1,106 @@
+-------------------------------
+--    "Sky" awesome theme    --
+--  By Andrei "Garoth" Thorp --
+-------------------------------
+
+theme = {}
+theme.font          = "Terminus 8"
+theme.hilight	    = "#FFCC44"
+
+theme.bg_focus      = "#e2eeea"
+theme.bg_normal     = "#729fcf"
+theme.bg_urgent     = "#fce94f"
+theme.bg_minimize   = "#0067ce"
+
+theme.fg_normal     = "#2e3436"
+theme.fg_focus      = "#2e3436"
+theme.fg_urgent     = "#2e3436"
+theme.fg_minimize   = "#2e3436"
+
+theme.border_width  = "2"
+theme.border_normal = "#dae3e0"
+theme.border_focus  = "#729fcf"
+theme.border_marked = "#eeeeec"
+
+theme.bg_widget           = "#333333"
+theme.fg_widget           = "#908884"
+theme.fg_center_widget    = "#636363"
+theme.fg_end_widget       = "#ffffff"
+theme.fg_off_widget       = "#22211f"
+
+theme.taglist_squares_sel = "/usr/share/awesome/themes/default/taglist/squarefw.png"
+theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
+
+theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+
+theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
+theme.menu_height   = "15"
+theme.menu_width    = "100"
+
+-- Define the image to load
+theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+
+theme.wallpaper_cmd         = { "awsetbg /usr/share/awesome/themes/sky/sky-background.png" }
+
+theme.layout_fairh      = "/home/isaiah/.config/awesome/themes/dust/layouts/fairhw.png"
+theme.layout_fairv      = "/home/isaiah/.config/awesome/themes/dust/layouts/fairvw.png"
+theme.layout_floating   = "/home/isaiah/.config/awesome/themes/dust/layouts/floatingw.png"
+theme.layout_magnifier  = "/home/isaiah/.config/awesome/themes/dust/layouts/magnifierw.png"
+theme.layout_max        = "/home/isaiah/.config/awesome/themes/dust/layouts/maxw.png"
+theme.layout_fullscreen = "/home/isaiah/.config/awesome/themes/dust/layouts/fullscreenw.png"
+theme.layout_tilebottom = "/home/isaiah/.config/awesome/themes/dust/layouts/tilebottomw.png"
+theme.layout_tileleft   = "/home/isaiah/.config/awesome/themes/dust/layouts/tileleftw.png"
+theme.layout_tile       = "/home/isaiah/.config/awesome/themes/dust/layouts/tilew.png"
+theme.layout_tiletop    = "/home/isaiah/.config/awesome/themes/dust/layouts/tiletopw.png"
+theme.layout_spiral     = "/home/isaiah/.config/awesome/themes/dust/layouts/spiralw.png"
+theme.layout_dwindle    = "/home/isaiah/.config/awesome/themes/dust/layouts/dwindlew.png"
+
+theme.awesome_icon16 = "/home/isaiah/.config/awesome/icons/awesome16-dust.png"
+theme.awesome_icon14 = "/home/isaiah/.config/awesome/icons/awesome14-dust.png"
+theme.arch_icon      = "/home/isaiah/.config/awesome/icons/arch2.png"
+theme.separator_icon      = "/home/isaiah/.config/awesome/icons/link2.png"
+
+theme.widget_cpu = "/home/isaiah/.config/awesome/icons/dust/cpu.png"
+theme.widget_mem = "/home/isaiah/.config/awesome/icons/dust/ram.png"
+theme.widget_bat = "/home/isaiah/.config/awesome/icons/dust/bat.png"
+theme.widget_ac = "/home/isaiah/.config/awesome/icons/dust/ac.png"
+theme.widget_crit = "/home/isaiah/.config/awesome/icons/dust/crit.png"
+theme.widget_blank = "/home/isaiah/.config/awesome/icons/dust/blank.png"
+theme.widget_vol = "/home/isaiah/.config/awesome/icons/dust/vol.png"
+theme.widget_mute = "/home/isaiah/.config/awesome/icons/dust/mute.png"
+theme.widget_pac = "/home/isaiah/.config/awesome/icons/dust/pac.png"
+theme.widget_upg = "/home/isaiah/.config/awesome/icons/dust/upg.png"
+theme.widget_mail = "/home/isaiah/.config/awesome/icons/dust/mail.png"
+theme.widget_mailn = "/home/isaiah/.config/awesome/icons/dust/mailn.png"
+theme.widget_temp = "/home/isaiah/.config/awesome/icons/dust/temp.png"
+theme.widget_wifi = "/home/isaiah/.config/awesome/icons/dust/wifi.png"
+theme.widget_nowifi = "/home/isaiah/.config/awesome/icons/dust/nowifi.png"
+theme.widget_up = "/home/isaiah/.config/awesome/icons/dust/up.png"
+theme.widget_down = "/home/isaiah/.config/awesome/icons/dust/down.png"
+theme.widget_weather = "/home/isaiah/.config/awesome/icons/dust/weather.png"
+theme.widget_mpd = "/home/isaiah/.config/awesome/icons/dust/mpd2.png"
+
+return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
